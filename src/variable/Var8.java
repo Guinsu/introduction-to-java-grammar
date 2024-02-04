879abb61a6c740a65a8d9e2b42ae50a7dba43bb6
Guinsu <guinsu.1996@gmail.com>
2024-02-04 14:42:56 +0900

    3st commit

---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+
+        //정수
+
+        byte b = 127; // -128~127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2146483647; // -2,147,483,648 ~ 2,147,483,648 약 20억
+        long l = 12312312123123L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
