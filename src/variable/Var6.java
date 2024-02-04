cde0050a008aeb9247056d13bd865d80cd5e1256
Guinsu <guinsu.1996@gmail.com>
2024-02-04 13:20:59 +0900

    "first commit"

---
+++ src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;  // 변수 선언만 하고 초기화를 하지 않으면 오류 발생 / 자바는 변수를 초기화 하도록 강제한다.
+        //위는 지역변수
+//        System.out.println(a); // 이 애러는 컴파일 애러이다.
+    }
+}
---
+++ src/variable/Var7.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수타입 리터럴 = 100
+        double b = 10.5; // 실수타입 리터럴 = 10.5
+        boolean c = true; // 불리언(boolean) true, false 입력가능 불리언 타입 리터럴 = true
+        char d = 'A'; // 문자하나
+        String e = "Hello java"; // 문자열, 문자열을 다루기 위한 특별한 타입
+
+        //리터럴은 개발자가 변수에 선언하는 값들을 리터럴이라고 한다.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
